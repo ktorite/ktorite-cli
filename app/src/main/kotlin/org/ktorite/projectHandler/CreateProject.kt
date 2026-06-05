77fee921ebe154e9cbd82f2aaca0d44fc6c90c86
rajeev-k47 <rajeevkumawat2007@outlook.com>
2026-06-05 15:14:10 +0530

    Bundle Gradle wrapper as resources in jar

--- app/src/main/kotlin/org/ktorite/projectHandler/CreateProject.kt
+++ app/src/main/kotlin/org/ktorite/projectHandler/CreateProject.kt
@@ -3,8 +3,6 @@ package org.ktorite.projectHandler
 import org.ktorite.helpers.loadManifest
 import org.ktorite.helpers.loadTemplate
 import java.io.File
-import java.nio.file.Files
-import java.nio.file.StandardCopyOption
 
 fun createProject(projectName: String, projectDirectory: String) {
   val targetDir = File(projectDirectory)
@@ -25,34 +23,26 @@ fun createProject(projectName: String, projectDirectory: String) {
     println("Created File: ${outputFile.path}")
   }
 
-  // Copy Gradle wrapper from CLI's own wrapper
+  // Copy Gradle wrapper from bundled resources
   val wrapperDir = File(targetDir, "gradle/wrapper")
   wrapperDir.mkdirs()
 
-  val sourceJar = File("../gradle/wrapper/gradle-wrapper.jar")
-  val sourceProps = File("../gradle/wrapper/gradle-wrapper.properties")
-  if (sourceJar.exists()) {
-    Files.copy(sourceJar.toPath(), File(wrapperDir, "gradle-wrapper.jar").toPath(), StandardCopyOption.REPLACE_EXISTING)
-    println("Created File: ${File(wrapperDir, "gradle-wrapper.jar")}")
-  }
-  if (sourceProps.exists()) {
-    sourceProps.copyTo(File(wrapperDir, "gradle-wrapper.properties"), overwrite = true)
-    println("Created File: ${File(wrapperDir, "gradle-wrapper.properties")}")
+  fun writeResource(path: String, destFile: File) {
+    val bytes = object {}.javaClass.getResourceAsStream(path)?.readAllBytes()
+    if (bytes != null) {
+      destFile.writeBytes(bytes)
+      println("Created File: ${destFile.path}")
+    }
   }
 
-  // Copy gradlew scripts from CLI root
-  val gradlew = File("../gradlew")
-  val gradlewBat = File("../gradlew.bat")
-  if (gradlew.exists()) {
-    val dest = File(targetDir, "gradlew")
-    gradlew.copyTo(dest, overwrite = true)
-    dest.setExecutable(true)
-    println("Created File: ${dest.path}")
-  }
-  if (gradlewBat.exists()) {
-    gradlewBat.copyTo(File(targetDir, "gradlew.bat"), overwrite = true)
-    println("Created File: ${File(targetDir, "gradlew.bat")}")
-  }
+  writeResource("/wrapper/gradle-wrapper.jar", File(wrapperDir, "gradle-wrapper.jar"))
+  writeResource("/wrapper/gradle-wrapper.properties", File(wrapperDir, "gradle-wrapper.properties"))
+
+  val gradlewFile = File(targetDir, "gradlew")
+  writeResource("/wrapper/gradlew", gradlewFile)
+  gradlewFile.setExecutable(true)
+
+  writeResource("/wrapper/gradlew.bat", File(targetDir, "gradlew.bat"))
 
   println("Project '$projectName' created successfully at $projectDirectory.")
 }
